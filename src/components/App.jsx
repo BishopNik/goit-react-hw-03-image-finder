d014bfddbcae6cbd2271f902520d7c977f5d32e4
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-09-26 22:56:45 +0200

    все функции в App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,23 @@
 /** @format */
 
 import { Component } from 'react';
-import PropTypes from 'prop-types';
+import {
+	BsFillSkipForwardFill,
+	BsFillSkipBackwardFill,
+	BsArrowRightSquareFill,
+	BsArrowLeftSquareFill,
+} from 'react-icons/bs';
+import { Notify } from 'notiflix';
+import { fetchImage } from './service/fetch_api';
+import Button from './button';
+import Loader from './loader';
 import Searchbar from './searchbar';
 import Gallery from './gallery';
 import Modal from './modal';
+import { ErrorComponent } from './service/error';
 
 import './style.css';
+import './gallery/style.css';
 
 class App extends Component {
 	state = {
@@ -14,15 +25,94 @@ class App extends Component {
 		page: 1,
 		isModalShow: false,
 		isNewSearch: false,
+		pageStart: 1,
 		bigImgShow: '',
+		value: '',
+		perPage: 12,
+		foundImages: [],
+		countFoundItem: 0,
+		countPage: 0,
+		statusComponent: null,
+		error: null,
 	};
 
-	static propTypes = {
-		bigImgShow: PropTypes.string,
+	componentDidUpdate = (prevProps, prevState) => {
+		const { page, perPage, countPage, searchItem, isNewSearch, pageStart } = this.state;
+		if (
+			prevState.searchItem !== searchItem ||
+			(prevState.isNewSearch !== isNewSearch && isNewSearch === true) ||
+			prevState.page !== page ||
+			prevState.perPage !== perPage
+		) {
+			const currentPage = searchItem !== prevState.searchItem ? pageStart : page;
+			this.setState({
+				statusComponent: 'pending',
+				searchItem,
+				page: prevState.perPage !== perPage ? 1 : currentPage,
+			});
+			fetchImage({
+				searchItem,
+				page: prevState.perPage !== perPage ? 1 : page,
+				perPage,
+			})
+				.then(({ hits, totalHits }) => {
+					const foundImages = [];
+					hits.forEach(({ id, webformatURL, largeImageURL, tags }) => {
+						if (id && webformatURL && largeImageURL && tags) {
+							foundImages.push({ id, webformatURL, largeImageURL, tags });
+						}
+					});
+					const pages = page === 1 ? Math.ceil(totalHits / perPage) : countPage;
+					this.setState(prevState => ({
+						...prevState,
+						foundImages,
+						countFoundItem: totalHits,
+						countPage: pages,
+						statusComponent: 'resolved',
+						value: perPage,
+					}));
+				})
+				.catch(({ message }) => {
+					this.setState({
+						statusComponent: 'rejected',
+						error: message,
+					});
+					Notify.failure('Unable to load results. ' + message);
+				})
+				.finally(this.handlerSearchComplete());
+		}
+	};
+
+	changePage = pg => {
+		const { countPage } = this.state;
+		this.setState(
+			prevState => {
+				const newPage = prevState.page + pg;
+				if (1 <= newPage && newPage <= countPage) {
+					return {
+						page: newPage,
+					};
+				}
+				return null;
+			},
+			() => {
+				const { page, countPage } = this.state;
+				switch (page) {
+					case 1:
+						Notify.info('First page');
+						return;
+					case countPage:
+						Notify.info('Last page');
+						return;
+					default:
+						break;
+				}
+			}
+		);
 	};
 
 	handlerChangeSearchValue = value => {
-		this.setState({ searchItem: value, isNewSearch: true });
+		this.setState({ searchItem: value, isNewSearch: true, pageStart: 1 });
 	};
 
 	handlerSearchComplete = value => {
@@ -37,8 +127,33 @@ class App extends Component {
 		this.setState(({ isModalShow }) => ({ isModalShow: !isModalShow }));
 	};
 
+	handlerChangeCountItem = ({ target }) => {
+		this.setState({ value: target.value.trim() });
+	};
+
+	handlerSubmitCountItem = e => {
+		if (e.key === 'Enter') {
+			e.preventDefault();
+			const { value } = this.state;
+			if (value) {
+				this.setState({ perPage: parseInt(value) });
+			}
+		}
+	};
+
 	render() {
-		const { isModalShow, searchItem, page, isNewSearch, bigImgShow } = this.state;
+		const {
+			searchItem,
+			page,
+			countPage,
+			countFoundItem,
+			statusComponent,
+			foundImages,
+			error,
+			value,
+			isModalShow,
+			bigImgShow,
+		} = this.state;
 		return (
 			<div className='container'>
 				{isModalShow && (
@@ -46,14 +161,89 @@ class App extends Component {
 						<img src={bigImgShow} alt='Big Search Element' />
 					</Modal>
 				)}
+
 				<Searchbar handlerSearch={this.handlerChangeSearchValue} />
-				<Gallery
-					searchItem={searchItem}
-					pageStart={page}
-					isNewSearch={isNewSearch}
-					onClickBigImage={this.handleClick}
-					onSearchCompeted={this.handlerSearchComplete}
-				/>
+
+				{statusComponent === 'pending' && <Loader />}
+
+				{statusComponent === 'resolved' && (
+					<>
+						{foundImages.length > 0 ? (
+							<>
+								<Gallery images={foundImages} onClickBigImage={this.handleClick} />
+								<div className='status-container'>
+									{page > 0 && countPage > 0 && (
+										<div className='page-stat'>
+											<p className='page-count'>item in page:</p>
+											<input
+												type='number'
+												className='page-item'
+												value={value}
+												min={1}
+												max={countFoundItem >= 200 ? 200 : countFoundItem}
+												onChange={this.handlerChangeCountItem}
+												onKeyDown={this.handlerSubmitCountItem}
+											/>
+											<div className='page-count'>
+												page: {page} / {countPage}
+											</div>
+										</div>
+									)}
+									{countPage > 1 && (
+										<>
+											<Button
+												className={'loadmore'}
+												type={'button'}
+												onClick={() => {
+													this.setState({ page: 1 });
+													Notify.info('First page');
+												}}
+											>
+												<BsFillSkipBackwardFill />
+											</Button>
+											<Button
+												className={'loadmore'}
+												type={'button'}
+												onClick={() => this.changePage(-1)}
+											>
+												<BsArrowLeftSquareFill />
+											</Button>
+											{page !== countPage && (
+												<>
+													<Button
+														className={'loadmore'}
+														type={'button'}
+														onClick={() => this.changePage(1)}
+													>
+														<BsArrowRightSquareFill />
+													</Button>
+													<Button
+														className={'loadmore'}
+														type={'button'}
+														onClick={() => {
+															this.setState({ page: countPage });
+															Notify.info('Last page');
+														}}
+													>
+														<BsFillSkipForwardFill />
+													</Button>
+												</>
+											)}
+										</>
+									)}
+								</div>
+							</>
+						) : (
+							<ErrorComponent>
+								Images <span className='search-item'>{searchItem}</span> not found
+							</ErrorComponent>
+						)}
+					</>
+				)}
+
+				{statusComponent === 'rejected' && (
+					<ErrorComponent className={'error'}>{error}</ErrorComponent>
+				)}
 			</div>
 		);
 	}
--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -1,242 +1,39 @@
 /** @format */
 
-import { Component } from 'react';
 import PropTypes from 'prop-types';
-import {
-	BsFillSkipForwardFill,
-	BsFillSkipBackwardFill,
-	BsArrowRightSquareFill,
-	BsArrowLeftSquareFill,
-} from 'react-icons/bs';
-import { Notify } from 'notiflix';
-import { fetchImage } from '../service/fetch_api';
 import GalleryItem from '../galleryitem';
-import Button from '../button';
-import Loader from '../loader';
-import { ErrorComponent } from 'components/service/error';
 import './style.css';
 
-class Gallery extends Component {
-	state = {
-		searchItem: '',
-		value: '',
-		page: 1,
-		perPage: 12,
-		foundImages: [],
-		countFoundItem: 0,
-		countPage: 0,
-		statusComponent: null,
-		error: null,
-	};
-
-	static propTypes = {
-		searchItem: PropTypes.string.isRequired,
-		isNewSearch: PropTypes.bool.isRequired,
-		pageStart: PropTypes.number.isRequired,
-		onClickBigImage: PropTypes.func.isRequired,
-		onSearchCompeted: PropTypes.func.isRequired,
-	};
-
-	componentDidUpdate = (prevProps, prevState) => {
-		const { page, perPage, countPage } = this.state;
-		const { searchItem, pageStart, isNewSearch, onSearchCompeted } = this.props;
-		if (
-			prevProps.searchItem !== searchItem ||
-			(prevProps.isNewSearch !== isNewSearch && isNewSearch === true) ||
-			prevState.page !== page ||
-			prevState.perPage !== perPage
-		) {
-			const currentPage = searchItem !== prevProps.searchItem ? pageStart : page;
-			this.setState({
-				statusComponent: 'pending',
-				searchItem,
-				page: prevState.perPage !== perPage ? 1 : currentPage,
-			});
-			fetchImage({
-				searchItem,
-				page: prevState.perPage !== perPage ? 1 : page,
-				perPage,
-			})
-				.then(({ hits, totalHits }) => {
-					const foundImages = [];
-					hits.forEach(({ id, webformatURL, largeImageURL, tags }) => {
-						if (id && webformatURL && largeImageURL && tags) {
-							foundImages.push({ id, webformatURL, largeImageURL, tags });
-						}
-					});
-					const pages = page === 1 ? Math.ceil(totalHits / perPage) : countPage;
-					this.setState(prevState => ({
-						...prevState,
-						foundImages,
-						countFoundItem: totalHits,
-						countPage: pages,
-						statusComponent: 'resolved',
-						value: perPage,
-					}));
-				})
-				.catch(({ message }) => {
-					this.setState({
-						statusComponent: 'rejected',
-						error: message,
-					});
-					Notify.failure('Unable to load results. ' + message);
-				})
-				.finally(onSearchCompeted);
-		}
-	};
-
-	changePage = pg => {
-		const { countPage } = this.state;
-		this.setState(
-			prevState => {
-				const newPage = prevState.page + pg;
-				if (1 <= newPage && newPage <= countPage) {
-					return {
-						page: newPage,
-					};
-				}
-				return null;
-			},
-			() => {
-				const { page, countPage } = this.state;
-				switch (page) {
-					case 1:
-						Notify.info('First page');
-						return;
-					case countPage:
-						Notify.info('Last page');
-						return;
-					default:
-						break;
-				}
-			}
-		);
-	};
-
-	handleClick = ({ target }) => {
+const Gallery = ({ images, onClickBigImage }) => {
+	const handleClick = ({ target }) => {
 		const bigImageSrc = target.dataset.largeurl;
-		this.props.onClickBigImage(bigImageSrc);
+		onClickBigImage(bigImageSrc);
 	};
+	return (
+		<ul className='gallery-container'>
+			{images.map(item => (
+				<GalleryItem
+					key={item.id}
+					srcUrl={item.webformatURL}
+					dataset={item.largeImageURL}
+					tags={item.tags}
+					onClick={handleClick}
+				/>
+			))}
+		</ul>
+	);
+};
 
-	handlerChangeCountItem = ({ target }) => {
-		this.setState({ value: target.value.trim() });
-	};
-
-	handlerSubmitCountItem = e => {
-		if (e.key === 'Enter') {
-			e.preventDefault();
-			const { value } = this.state;
-			if (value) {
-				this.setState({ perPage: parseInt(value) });
-			}
-		}
-	};
-
-	render() {
-		const {
-			page,
-			countPage,
-			searchItem,
-			countFoundItem,
-			statusComponent,
-			foundImages,
-			error,
-			value,
-		} = this.state;
-		if (statusComponent === 'pending') {
-			return <Loader />;
-		}
-
-		if (statusComponent === 'resolved') {
-			return (
-				<>
-					<ul className='gallery-container'>
-						{foundImages.length > 0 ? (
-							foundImages.map(item => (
-								<GalleryItem
-									key={item.id}
-									srcUrl={item.webformatURL}
-									dataset={item.largeImageURL}
-									tags={item.tags}
-									onClick={this.handleClick}
-								/>
-							))
-						) : (
-							<ErrorComponent>
-								Images <span className='search-item'>{searchItem}</span> not found
-							</ErrorComponent>
-						)}
-					</ul>
-
-					<div className='status-container'>
-						{page > 0 && countPage > 0 && (
-							<div className='page-stat'>
-								<p className='page-count'>item in page:</p>
-								<input
-									type='number'
-									className='page-item'
-									value={value}
-									min={1}
-									max={countFoundItem >= 200 ? 200 : countFoundItem}
-									onChange={this.handlerChangeCountItem}
-									onKeyDown={this.handlerSubmitCountItem}
-								/>
-								<div className='page-count'>
-									page: {page} / {countPage}
-								</div>
-							</div>
-						)}
-						{countPage > 1 && (
-							<>
-								<Button
-									className={'loadmore'}
-									type={'button'}
-									onClick={() => {
-										this.setState({ page: 1 });
-										Notify.info('First page');
-									}}
-								>
-									<BsFillSkipBackwardFill />
-								</Button>
-								<Button
-									className={'loadmore'}
-									type={'button'}
-									onClick={() => this.changePage(-1)}
-								>
-									<BsArrowLeftSquareFill />
-								</Button>
-								{page !== countPage && (
-									<>
-										<Button
-											className={'loadmore'}
-											type={'button'}
-											onClick={() => this.changePage(1)}
-										>
-											<BsArrowRightSquareFill />
-										</Button>
-										<Button
-											className={'loadmore'}
-											type={'button'}
-											onClick={() => {
-												this.setState({ page: countPage });
-												Notify.info('Last page');
-											}}
-										>
-											<BsFillSkipForwardFill />
-										</Button>
-									</>
-								)}
-							</>
-						)}
-					</div>
-				</>
-			);
-		}
-
-		if (statusComponent === 'rejected') {
-			return <ErrorComponent className={'error'}>{error}</ErrorComponent>;
-		}
-	}
-}
+Gallery.propTypes = {
+	images: PropTypes.arrayOf(
+		PropTypes.exact({
+			id: PropTypes.number.isRequired,
+			webformatURL: PropTypes.string.isRequired,
+			largeImageURL: PropTypes.string.isRequired,
+			tags: PropTypes.string.isRequired,
+		})
+	).isRequired,
+	onClickBigImage: PropTypes.func.isRequired,
+};
 
 export default Gallery;
