8a677a1514cd6ce5a778e4f9854acb41f76d6c25
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-09-27 10:16:08 +0200

    edit galleryItem

--- src/components/galleryitem/galleryitem.jsx
+++ src/components/galleryitem/galleryitem.jsx
@@ -3,15 +3,10 @@
 import PropTypes from 'prop-types';
 import './style.css';
 
-const GalleryItem = () => {
+const GalleryItem = ({ srcUrl, tags, dataset, onClick }) => {
 	return (
-		<li className='gallery-item' onClick={this.props.onClick}>
-			<img
-				className='gallery-img'
-				src={this.props.srcUrl}
-				alt={this.props.tags}
-				data-largeurl={this.props.dataset}
-			/>
+		<li className='gallery-item' onClick={onClick}>
+			<img className='gallery-img' src={srcUrl} alt={tags} data-largeurl={dataset} />
 		</li>
 	);
 };
