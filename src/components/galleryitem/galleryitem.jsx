7499a259d329875ff71fb50d470ac3132b1b5bf6
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-09-27 10:11:56 +0200

    class на func

--- src/components/galleryitem/galleryitem.jsx
+++ src/components/galleryitem/galleryitem.jsx
@@ -1,29 +1,26 @@
 /** @format */
 
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 
-class GalleryItem extends Component {
-	static propTypes = {
-		srcUrl: PropTypes.string.isRequired,
-		tags: PropTypes.string.isRequired,
-		dataset: PropTypes.string.isRequired,
-		onClick: PropTypes.func.isRequired,
-	};
+const GalleryItem = () => {
+	return (
+		<li className='gallery-item' onClick={this.props.onClick}>
+			<img
+				className='gallery-img'
+				src={this.props.srcUrl}
+				alt={this.props.tags}
+				data-largeurl={this.props.dataset}
+			/>
+		</li>
+	);
+};
 
-	render() {
-		return (
-			<li className='gallery-item' onClick={this.props.onClick}>
-				<img
-					className='gallery-img'
-					src={this.props.srcUrl}
-					alt={this.props.tags}
-					data-largeurl={this.props.dataset}
-				/>
-			</li>
-		);
-	}
-}
+GalleryItem.propTypes = {
+	srcUrl: PropTypes.string.isRequired,
+	tags: PropTypes.string.isRequired,
+	dataset: PropTypes.string.isRequired,
+	onClick: PropTypes.func.isRequired,
+};
 
 export default GalleryItem;
